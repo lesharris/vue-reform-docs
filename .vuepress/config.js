49d1d86b81fd7bc80f46246512731063400a9fd6
Les Harris <les@lesharris.com>
2019-03-25 23:27:01 -0700

    Initial

---
+++ .vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Vue Reform Documentation',
+  description: 'Documentation for Vue Reform',
+  plugins: [
+    '@vuepress/theme-default',
+    '@vuepress/clean-urls',
+    '@vuepress/container',
+    '@vuepress/register-components',
+    '@vuepress/active-header-links',
+    '@vuepress/search',
+    '@vuepress/nprogress',
+    '@vuepress/back-to-top',
+    '@vuepress/pagination',
+    '@vuepress/pwa',
+    '@vuepress/medium-zoom'
+  ]
+}
